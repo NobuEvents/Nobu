7a51d915cef1de58bafa125c3f0a5945aaebae93
Ananth Packkildurai <apackkildurai@gmail.com>
2022-11-23 11:16:08 -0500

    Sample Source and Target implementation

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -1,1 +1,19 @@
 package router
+
+type Routes struct {
+	NobuType map[string]*TargetRoute
+	Target   map[string]*TargetRoute
+}
+
+func NewRoutes() *Routes {
+	return &Routes{}
+}
+
+type TargetRoute struct {
+	Topic string
+}
+
+// NobuTypeToTargetRoute TODO: pending to implement
+func (r *Routes) NobuTypeToTargetRoute(NobuType string) *TargetRoute {
+	return nil
+}
