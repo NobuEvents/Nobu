3aaf5b061bb0f0f607521cd8bde8098172acd50b
Ananth Packkildurai <apackkildurai@gmail.com>
2023-05-25 13:46:01 -0400

    adding a healthcheck for Nobu Server

---
+++ server/src/main/java/com/nobu/server/HealthCheckServer.java
@@ -0,0 +1,16 @@
+package com.nobu.server;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@Liveness
+@ApplicationScoped
+public class HealthCheckServer implements HealthCheck {
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Server is running");
+    }
+}
