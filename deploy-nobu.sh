9d80a006ed97386339a77981502d9873dbd27cf9
sathish1993 <sathish_kumar_93@yahoo.com>
2023-05-25 18:55:39 -0500

    sleep 15s before applying latest config

--- deploy-nobu.sh
+++ deploy-nobu.sh
@@ -7,10 +7,11 @@ docker push gcr.io/just-site-344717/nobu:$TAG
 
 sed -e "s/tag/"$TAG"/g" kube.yaml > kube_latest.yaml
 kubectl delete deploy nobu-dep
+sleep 15
 kubectl apply -f kube_latest.yaml
 rm kube_latest.yaml
 
-sleep 60
+sleep 30
 POD_NAME=$(kubectl get pods | grep "nobu" | awk '{ print $1 }')
 kubectl port-forward $POD_NAME 8080:8080
 #open http://localhost:8080/q/health/live
